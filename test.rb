804838c15820ee03804af2a5373dcd6fe607b5e6
Ahsan-Ali098 <ahsansaleem098@gmail.com>
2022-08-08 20:18:49 +0500

    First commit

---
+++ test.rb
@@ -0,0 +1,113 @@
+#puts "Hello"
+
+#Methods
+# def add_numbers(num1,num2=99)
+#     return num1+num2
+# end
+
+
+# sum = add_numbers(10)
+# puts sum
+
+# test_grades ={
+#     "Andy" => 'B+',
+#     "Ahsan" => 'B+',
+#     :Stanley => 'A+',
+#     3 => 95.2
+# }
+#Hashing/Dict
+
+# test_grades['Andy']= 'F'
+# puts test_grades['Andy']
+# puts test_grades[:Stanley]
+# puts test_grades[3]
+# puts test_grades['Ahsan']
+
+# arr = [1,4,5,3,4]
+
+# index = 2
+# while index <= 1000000000000000000000000000000
+#     puts index
+#     index +=1
+    
+# end
+
+# for index in 0..5
+#     puts index
+# end
+
+# 1000000000000000000000.times do |index|
+#     puts index
+# end
+
+# print'Hello' #yeh new line mai nai lyk jata 
+# puts'hello'   #yeh new mai ly k jata hai 
+
+# puts 'Enter a number?'
+# num1 = gets.chomp         #simple gets always recieve a enter with it 
+# puts "hello #{num1}, how are you?"
+
+# x=3
+# puts "x is NOT 3" unless x == 3
+
+# def string_length_interpolater(incoming_string)
+#     length=incoming_string.length
+#     puts "The string you just gave me has a length of #{length}"
+# end
+# a = 'hello world'
+# string_length_interpolater(a)
+# puts 5 + 6
+
+# puts "I can't work with any other language but Ruby".end_with? "Ruby"
+
+# puts "I am a Rubyist".index 'R'
+
+# addition = lambda {|a, b| return a + b }
+# puts addition.call(5, 6)
+# arr =[2,3,4,5,6,7,8,8,8,8,8,8,8,8,8,8]
+# for i in 110..0
+#     puts i
+
+# end
+
+# a = 1
+# b = 4
+# puts "The number #{a} is less than #{b}"  
+
+
+# string = 'Hello this string needs to be checked with splitt' 
+
+# puts string.split '/n'
+
+# def sayhi(name,age)
+#     age = age.to_s
+#     puts "Hello " + name + ",you are " + age
+#     puts name.class
+#     puts age.class
+
+# end 
+
+# sayhi("Saira", 73)
+
+# l = lambda do |string|
+#     if string == "try"
+#       return "There's no such thing" 
+#     else
+#       return "Do or do not."
+#     end
+#   end
+#   puts l.call("try") # Feel free to experiment with this
+
+arr = [3,2,3,4,5,6,5,4,4,3,32]
+arr
+len = arr.length
+
+for i in 0..len
+    count = 1
+    for j in i+1..len
+        if arr[i] == arr[j]
+            count +=1
+        end
+    end
+    puts "THe number #{arr[i]} is #{count} times"
+end
